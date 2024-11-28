7a2886c9e2f5cd1e3c2617cf4dce08b07b4793b8
falcon-guy <farukon-gai@ezweb.ne.jp>
2024-11-28 21:13:28 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,21 @@
+ // 変数の宣言(体重)
+ let Bodyweight;
+
+ // 変数の宣言(身長)
+ let Height;
+
+ // 値の代入(体重)
+ Bodyweight = 68;
+
+ // 値の代入(身長)
+ Height = 1.7;
+ 
+ // 変数の宣言(BMI)
+ let Bmi;
+
+ // 値の代入(身長)
+ Bmi = Bodyweight / (Height*Height);
+ 
+ 
+ // 体重と身長のBMI（体格指数）を出力
+ console.log(Bmi);
